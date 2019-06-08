bcb628b5de7ae8f9f757ca259dc078a09a6c1039
Adrian Popan <adrian.popan@gmail.com>
2019-06-08 21:12:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,11 @@ def countdown(x)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(x)
+  
+  while x > 0 
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
   
 end
